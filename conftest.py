b3c65b2fb272d4db3055bf213c426c17b57106c1
Kirill Martyushov <martyushov.k.i.m@gmail.com>
2025-12-20 18:14:16 +0300

    Исправление замечания по фикстурам

--- conftest.py
+++ conftest.py
@@ -3,6 +3,12 @@ from main import BooksCollector
 from books_data import BOOK_WITH_GENRE
 
 
+# Фикстура для создания пустого эксземпляра BooksCollector
+@pytest.fixture
+def collector():
+    return BooksCollector()
+
+# Фикстура для создания экземпляра BooksCollector с предзаполненными книгами и жанрами
 @pytest.fixture
 def collector_with_books():
     collector = BooksCollector()
